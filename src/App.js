30ef902da6a7e7af69d1c2f91ef3d9d71e3fcf07
matt <matthieumariejoseph@gmail.com>
2020-07-27 20:02:59 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/Soccer Bet.png';
 import './App.css';
+import Competitions from './components/Competitions';
+import Bets from './components/Bets';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require('dotenv').config();
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      resetCompetition : null,
+      newBet : true
+    }
+
+    this.getCompetitionReset = this.getCompetitionReset.bind(this);
+    this.resetCompetition = this.resetCompetition.bind(this);
+    this.getVue = this.getVue.bind(this);
+  }
+
+  resetCompetition() {
+    this.setState({
+      newBet : true
+    });
+    this.state.resetCompetition();
+  }
+
+  getCompetitionReset(resetFunction) {
+    this.setState({
+      resetCompetition : resetFunction
+    })
+  }
+
+  getMyBets() {
+    this.setState({
+      newBet : false
+    })
+  }
+
+  getVue() {
+    if (this.state.newBet) {
+      return (<Competitions getCompetitionReset={this.getCompetitionReset}/>);
+    }
+    return (<Bets/>);
+  }
+
+  render() {return (
+      <div className="App">
+          <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+              <a class="navbar-item" href="#">
+                <img src="./Soccer Bet.png" class="image is-50x50" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="50" height="50"/>
+              </a>
+            </div>
+            <div class="navbar-menu">
+              <div class="navbar-end center mr-1">
+                <button className="button is-violet mr-1" onClick={() => this.getMyBets()}>My Bets</button>
+                <button className="button button-violet is-text-violet" onClick={() => this.resetCompetition()}>New Bets</button>
+              </div>
+            </div>
+          </nav>
+        <header>
+          {this.getVue()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/services/Web3Access.js
@@ -0,0 +1,22 @@
+import Web3 from 'web3';
+
+class Web3Access {
+
+    web3;
+
+    constructor () {
+        this.web3 = new Web3(window.web3.currentProvider);
+    }
+
+
+    static Instance;
+
+    static GetInstance = () => {
+        if(Web3Access.Instance === undefined) {
+            Web3Access.Instance = new Web3Access();
+        }
+        return Web3Access.Instance;
+    }
+}
+
+export default Web3Access;
